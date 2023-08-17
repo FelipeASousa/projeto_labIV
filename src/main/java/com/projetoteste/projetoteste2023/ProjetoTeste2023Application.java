eb8ec48a06ff8b69706c2a7d69969f8b625f9923
FelipeASousa <feaugusto893@gmail.com>
2023-08-16 22:01:35 -0300

    primeiro commit

---
+++ src/main/java/com/projetoteste/projetoteste2023/ProjetoTeste2023Application.java
@@ -0,0 +1,13 @@
+package com.projetoteste.projetoteste2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTeste2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTeste2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoteste/projetoteste2023/ProjetoTeste2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoteste.projetoteste2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTeste2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
