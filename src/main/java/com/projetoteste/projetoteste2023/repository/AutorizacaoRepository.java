2d6b49d050f7ae01bcedd25ade7ef221722e2f63
Felipe Sousa <feaugusto893@GMAIL.COM>
2023-08-30 00:07:03 +0000

    Criação do Repositório de Anotação

---
+++ src/main/java/com/projetoteste/projetoteste2023/repository/AutorizacaoRepository.java
@@ -0,0 +1,9 @@
+package com.projetoteste.projetoteste2023.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetoteste.projetoteste2023.entity.Autorizacao;
+
+public interface AutorizacaoRepository extends JpaRepository<Autorizacao, Long>{
+    
+}
--- src/main/java/com/projetoteste/projetoteste2023/service/UsuarioService.java
+++ src/main/java/com/projetoteste/projetoteste2023/service/UsuarioService.java
@@ -1,34 +1,64 @@
 package com.projetoteste.projetoteste2023.service;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.projetoteste.projetoteste2023.entity.Autorizacao;
 import com.projetoteste.projetoteste2023.entity.Usuario;
+import com.projetoteste.projetoteste2023.repository.AutorizacaoRepository;
 import com.projetoteste.projetoteste2023.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
+
+    // Repositorios
     @Autowired
     private UsuarioRepository usuarioRepo;
+    @Autowired
+    private AutorizacaoRepository autorizacaorep;
+    // Funções
+    public Autorizacao buscarPorAutorizacaoId(Long id) {
+        Optional<Autorizacao> autorizacaoOp = autorizacaorep.findById(id);
+        if(autorizacaoOp.isPresent()) {
+            return autorizacaoOp.get();
+        }
+        throw new IllegalArgumentException("Id inválido!");
+    }
+    @Transactional
     public Usuario buscarPorId(Long id) {
         Optional<Usuario> usuarioOp = usuarioRepo.findById(id);
         if(usuarioOp.isPresent()) {
- return usuarioOp.get();
- }
- throw new IllegalArgumentException("Id inválido!");
- }
- public Usuario novoUsuario(Usuario usuario) {
- if(usuario == null ||
- usuario.getNome() == null ||
- usuario.getSenha() == null) {
- throw new IllegalArgumentException("Nome e senha inválidos!");
- }
- return usuarioRepo.save(usuario);
- }
- public List<Usuario> buscarTodos() {
- return usuarioRepo.findAll();
- }
-}
+             return usuarioOp.get();
+        }
+        throw new IllegalArgumentException("Id inválido!");
+    }
+
+    public Usuario novoUsuario(Usuario usuario) {
+        if(usuario == null ||
+            usuario.getNome() == null ||
+            usuario.getSenha() == null) {
+        throw new IllegalArgumentException("Nome e senha inválidos!");
+        }
+        if(!usuario.getAutorizacoes().isEmpty()){
+            Set<Autorizacao> autorizacoes = new HashSet<Autorizacao>();
+            for (Autorizacao autorizacao: usuario.getAutorizacoes()){
+                Autorizacao autorizacaoBd = buscarPorAutorizacaoId(autorizacao.getId());
+                autorizacoes.add(autorizacaoBd);
+            }
+            usuario.setAutorizacoes(autorizacoes);
+        }
+        return usuarioRepo.save(usuario);
+    }
+
+    public List<Usuario> buscarTodos() {
+        return usuarioRepo.findAll();
+    }
+
+
+    }
